c726d0f3b30e843a4391b6d8df2a47c8d6ec777f
sarthakroy107 <sarthakroy107@gmail.com>
2023-06-17 23:29:44 +0530

    Merge Intervals

---
+++ Array-1/56.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        for(int i = 0; i < intervals.size(); i++) {
+            if(ans.empty() || intervals[i][0] > ans.back()[1]) ans.push_back(intervals[i]);
+            else {
+                ans.back()[1] = max(intervals[i][1], ans.back()[1]);
+            }
+        }
+        return ans;
+    }
+};
